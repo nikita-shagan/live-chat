73e2aa62f9e85ddbbaf3e327c9826d8b11b33cc8
nikita-shagan <face9100@gmail.com>
2023-10-25 01:22:42 +0500

    fix: chage cors to localhost for local setup

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const httpServer = http.createServer();
 
 const io = new Server(httpServer, {
   cors: {
-    origin: "https://chat.shagan.ru",
+    origin: "http://localhost:3000",
     methods: ["GET", "POST"],
     allowedHeaders: ["my-custom-header"],
     credentials: true,
